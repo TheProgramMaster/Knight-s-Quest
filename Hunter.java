8d7fd32264b80dd69b70ddd5c8be099989014fc5
TheProgramMaster <72845454+TheProgramMaster@users.noreply.github.com>
2022-01-29 22:05:11 -0600

    Created Hunter Information Class

--- Hunter.java
+++ Hunter.java
@@ -8,4 +8,29 @@ public class Hunter {
 		this.strength = strength;
 		this.defense = defense;
 	}
+	public int getHealth() {
+		return this.health;
+	}
+	public void getHealth(int health) {
+		this.health = health;
+	}
+	public int getStrength() {
+		return this.strength;
+	}
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	public int getDefense() {
+		return this.defense;
+	}
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+	public String toString() {
+		String result = "";
+		result += "Health of Hunter: " + this.getHealth() + "\n" +
+		"Strength of Hunter: " + this.getStrength() + "\n" + 
+		"Defense of Hunter: " + this.getStrength();
+		return result;
+	}
 }
